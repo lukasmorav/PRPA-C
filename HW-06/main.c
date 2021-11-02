bd68c4fd93b9502fc64b96cc1ccbadbc369b4eb3
lukasmorav <moravec.l@outlook.cz>
2021-11-02 17:13:16 +0100

    06-message loader

--- HW-06/main.c
+++ HW-06/main.c
@@ -1,38 +1,14 @@
 #include <stdio.h>
-#include <string.h>
+
 
 
 int main()
 {
     //message loader
-    int inputLength, scanfRet;
-    if (scanf("%i", &inputLength) != 1)
-    {
-        fprintf(stderr, "Error: Chybny vstup!\n");
-        return 100;
-    }
+    int messageSize;
+    char *codedMessage, *halfDecodedMessage;
+    scanf("%i%s%s", &messageSize, &codedMessage, &halfDecodedMessage);
 
-    char inputData, decodedMessage, encodedMessage;
-    while (scanfRet = scanf("%s", &inputData) != EOF)
-    {
-        printf("%li", strlen(&inputData));
-        if (strlen(&inputData) != inputLength)
-        {
-            fprintf(stderr, "Error: Chybna delka vstupu!\n");
-            return 101;
-        }
-        
-        if (&encodedMessage == NULL)
-        {
-            encodedMessage = inputLength;
-            continue;
-        }
-        else
-        {
-            decodedMessage = inputData;
-            continue;
-        }
-    }
-
+    printf("%s\n", halfDecodedMessage);
     return 0;
 }
